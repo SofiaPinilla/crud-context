6be1e6943d164c30f0ba4db7fd2151bbb64b6947
SofiaPinilla <contact@sofiapinilla.com>
2022-02-21 16:40:17 +0100

    edit task functionality added & redirection

--- src/components/Tasks/EditTask/EditTask.jsx
+++ src/components/Tasks/EditTask/EditTask.jsx
@@ -1,34 +1,43 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams,useNavigate } from "react-router-dom";
 import { GlobalContext } from "../../../context/GlobalState";
 const EditTask = () => {
     const { task, getTask, editTask } = useContext(GlobalContext);
-    const [title, setTitle] = useState("...");
+    const [title, setTitle] = useState("");
     const { _id } = useParams();
-  const [params, setParams] = useState("");
+    let navigate = useNavigate();
+
+    // const [color, setColor] = useState("red");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     editTask(task._id, { title });
+    setTimeout(() => {
+        navigate("/");
+      }, 1000);
   };
 
   useEffect(() =>   {
     getTask(_id)
+    // setTimeout(() => {
+    //     setColor('blue')
+    // }, 1000);
+  },[]);
+
+  useEffect(() =>   {
     setTitle(task.title)
-    console.log('task',task)
-        setParams(_id)
-    console.log('entras')
-    return ()=> setParams("")
-  },[params]  );
+  },[task.title]);
 
-
+//   useEffect(() =>   {
+//     console.log('color cambiado' + color)
+//   },[color]);
 
   return (
     <form onSubmit={handleSubmit}>
       <input
         type="text"
         onChange={(e) => setTitle(e.target.value)}
-        value={ title ||"" }
+        value={ title || ""}
         name="title"
       />
       <button type="submit">Edit task</button>
